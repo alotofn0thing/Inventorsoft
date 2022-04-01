fe82fe27953a744a7f58062a9e89cadb9c992f57
Volodymyr Shum <shum.vr@gmail.com>
2022-04-01 21:19:02 +0300

    Add task1

--- git-lesson/task1/index.js
+++ git-lesson/task1/index.js
@@ -1,4 +1,22 @@
-function calculate() {
-  //Here is your solution
-  return
+const calculate = (string) => {
+
+  let [first, operator, second] = string.split(' ');
+
+ first = first.length;
+  
+ second = second.length;
+  
+  switch(operator){
+      case '+':
+          return '.'.repeat(first + second);
+      case '-':
+          return '.'.repeat(first - second);
+      case '*':
+          return '.'.repeat(first * second);              
+      case '//':
+          return '.'.repeat(first / second);
+      default: 
+          return '';
+  } 
+  
 }

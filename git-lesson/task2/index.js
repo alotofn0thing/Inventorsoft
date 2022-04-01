3f3b7706b021281191eea82e67231992b1ea7dd3
Volodymyr Shum <shum.vr@gmail.com>
2022-04-01 22:04:08 +0300

    Add task2

--- git-lesson/task2/index.js
+++ git-lesson/task2/index.js
@@ -1,4 +1,22 @@
-function calculate() {
-  //Here is your solution
-  return
+const calculate = (N) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+  if (N < 0) return null;
+
+  const col = [];
+
+  for (let i = 0; i < N; i++) {
+    const row = [];
+
+    for (let j = 0; j < N; j++) {
+      const index = (i + j) % alphabet.length;
+      const element = alphabet[index];
+
+      row.push(element);
+    }
+
+    col.push(row.join(' '));
+  }
+
+  return col.join('\n');
 }

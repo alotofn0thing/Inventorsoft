0f2258704759f817f1479569a7a363314b0d6a49
Volodymyr Shum <shum.vr@gmail.com>
2022-04-04 02:10:28 +0300

    Add task 10 js

---
+++ JavaScript/task10.js
@@ -0,0 +1,15 @@
+// Напиши функцию, считающую число свойств в объекте:
+
+// var a = { a: 1, b: 2 };
+// console.log(count(a)); // 2
+// var b = function () {};
+// console.log(count(b)); // 0
+// var c = [1, 2, 3];
+// console.log(count(c)); // 3
+// var d = [];
+// d[100] = 1;
+// console.log(count(d)); // 1
+// Кстати, в новых браузерах с поддержкой Javascript ES5 есть метод Object.keys(x), возвращающий массив ключей у объекта.
+
+
+const count = obj => Object.keys(obj).length;

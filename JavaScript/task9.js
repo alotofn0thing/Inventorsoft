ff229ac0de27ba9794802836ee493bb415de8ee3
Volodymyr Shum <shum.vr@gmail.com>
2022-04-07 03:55:34 +0300

    Refactoring task9 js

--- JavaScript/task9.js
+++ JavaScript/task9.js
@@ -8,5 +8,10 @@
 // console.log(input); // [1, 2, 3, 4, 5, 6]
 // Аналог из lodash: http://lodash.com/docs#filter В новых браузерах у массивов есть метод filter.
 
-
-const filter = (arr, fn) => arr.filter(elem => fn(elem));
+const filter = (arr, func) => {
+    const res = [];
+    for (let item in arr) {
+        func(arr[item]) ? res.push(arr[item]) : null;
+    }
+    return res;
+};
